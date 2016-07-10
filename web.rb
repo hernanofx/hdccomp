214c8fde54cc501a86e1e1adde7ab5bbf61a6cf5
hernanofx <hernanofx@hotmail.com>
2016-07-10 11:30:54 -0300

    archivos y carpetas iniciales vacios

---
+++ web.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
+get '/home' do
+  erb :index
+end
+
+get '/particulares' do
+  erb :particulares
+end
+
+get '/empresas' do
+  erb :about
+end
+
+get '/presupuesto' do
+ erb :presupuesto
+ end
+
+get '/nosotros' do
+  erb :nosotros
+  end
+
+get '/' do
+  erb :layout
+end
