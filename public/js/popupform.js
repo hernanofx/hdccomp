696a7f812ae9e02e933f62de171ccc96055ff7e0
hernanofx <hernanofx@hotmail.com>
2016-07-21 18:47:03 -0300

    beatufy

--- public/js/popupform.js
+++ public/js/popupform.js
@@ -1,17 +1,17 @@
 // Validating Empty Field
 function check_empty() {
-if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-alert("Fill All Fields !");
-} else {
-document.getElementById('form').submit();
-alert("Form Submitted Successfully...");
-}
+    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+        alert("Fill All Fields !");
+    } else {
+        document.getElementById('form').submit();
+        alert("Form Submitted Successfully...");
+    }
 }
 //Function To Display Popup
 function div_show() {
-document.getElementById('abc').style.display = "block";
+    document.getElementById('abc').style.display = "block";
 }
 //Function to Hide Popup
-function div_hide(){
-document.getElementById('abc').style.display = "none";
+function div_hide() {
+    document.getElementById('abc').style.display = "none";
 }
--- public/js/slidef.js
+++ public/js/slidef.js
@@ -1,21 +1,24 @@
-setInterval('avanzaSlide()',5000);
-var arrayImagenes = new Array(".img1",".img2",".img3");
+setInterval('avanzaSlide()', 5000);
+var arrayImagenes = new Array(".img1", ".img2", ".img3");
 var contador = 0;
-function mostrar(img){
-  $(img).ready(function(){
-      $(arrayImagenes[contador]).fadeIn(1500);
-  });
+
+function mostrar(img) {
+    $(img).ready(function() {
+        $(arrayImagenes[contador]).fadeIn(1500);
+    });
 }
-function ocultar(img){
-  $(img).ready(function(){
-      $(arrayImagenes[contador]).fadeOut(1500);
-  });
+
+function ocultar(img) {
+    $(img).ready(function() {
+        $(arrayImagenes[contador]).fadeOut(1500);
+    });
 }
-function avanzaSlide(){
-        //se oculta la imagen actual
-  ocultar(arrayImagenes[contador]);
-        //aumentamos el contador en una unidad
-  contador = (contador + 1) % 3;
-        //mostramos la nueva imagen
-  mostrar(arrayImagenes[contador]);
+
+function avanzaSlide() {
+    //se oculta la imagen actual
+    ocultar(arrayImagenes[contador]);
+    //aumentamos el contador en una unidad
+    contador = (contador + 1) % 3;
+    //mostramos la nueva imagen
+    mostrar(arrayImagenes[contador]);
 }
