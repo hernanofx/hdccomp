99808c145f9447bb8ecf1a25234133c6817944fa
hernanofx <hernanofx@hotmail.com>
2016-08-03 15:55:16 -0300

    actualizacion

--- hdccomp/web.rb
+++ hdccomp/web.rb
@@ -25,10 +25,13 @@ get '/presupuesto' do
  erb :presupuesto
  end
 
- get '/presupuesto2' do
+get '/presupuesto2' do
   erb :presupuesto2
   end
 
+post '/client_form' do
+  "Presupuesto enviado! Verifique si estos datos son correctos: #{params[:primernombre]} #{params[:apellido]}, #{params[:telefono]}, #{params[:email]}, #{params[:calle]} #{params[:altura]}, #{params[:piso]}, #{params[:localidad]}"
+  end
 
 get '/nosotros' do
   erb :nosotros
