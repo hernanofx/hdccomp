734283b7ccc67f61ee7162950c097c9d5d1cefb2
hernanofx <hernanofx@hotmail.com>
2016-08-03 15:54:12 -0300

    actualización de compu y script ruby form

---
+++ views/datospersonales.rb
@@ -0,0 +1,30 @@
+print "¿Cuál es tu primer nombre?"
+primer_nombre = gets.chomp
+primer_nombre.capitalize!
+
+print "¿Cuál es tu apellido?"
+apellido = gets.chomp
+apellido.capitalize
+
+print "¿Cuál es tu telefono?"
+telefono = gets.chomp
+
+print "¿Cuál es tu email de contacto?"
+email_de_contacto = gets.chomp
+
+print "¿En que calle vivis?"
+calle = gets.chomp
+calle.capitalize
+
+print "¿Que altura?"
+altura = gets.chomp
+
+print "¿En que piso vivis?"
+piso = gets.chomp
+
+print "¿Que localidad?"
+localidad = gets.chomp
+localidad.capitalize
+
+puts "Tu nombre es #{primer_nombre} #{apellido}, vivis en #{calle} #{altura} piso #{piso} en la localidad de #{localidad}, tu teléfono es #{telefono} y tu email de contacto es #{email_de_contacto}"
+puts "Nos estaremos comunicando a la brevedad contigo #{primer_nombre}"
