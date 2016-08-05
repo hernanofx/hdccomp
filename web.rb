612e870c2be63b19209158a71b4b441385926b8e
hernanofx <hernanofx@hotmail.com>
2016-08-05 19:42:56 -0300

    color web-rb

--- web.rb
+++ web.rb
@@ -19,11 +19,11 @@ end
 
 get '/presupuesto2' do
   erb :presupuesto2
-  end
+end
 
-#get '/datospersonales' do
-#  erb :datospersonales
-#  end
+get '/datospersonales' do
+  erb :datospersonales
+end
 
 post '/client_form' do
   "Presupuesto enviado! Verifique si estos datos son correctos: #{params[:primernombre]} #{params[:apellido]}, #{params[:telefono]}, #{params[:email]}, #{params[:calle]} #{params[:altura]}, #{params[:piso]}, #{params[:localidad]}, nos comunicaremos con ud. a la brevedad"
